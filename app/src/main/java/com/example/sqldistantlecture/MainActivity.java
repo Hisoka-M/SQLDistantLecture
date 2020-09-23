6ca7a014cb4bda816bfd8c3ba0f1d36378a7dd9c
Hisoka <71383502+Hisoka-M@users.noreply.github.com>
2020-09-23 08:17:43 +0100

    Comments
    Commentaires ajoutés + affichage

--- app/src/main/java/com/example/sqldistantlecture/MainActivity.java
+++ app/src/main/java/com/example/sqldistantlecture/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
     InputStream is;
     // lien avec le PHP qui procède à la requête
     String urlWebService = "http://172.16.47.101/accessrvtowns/accesSrvTowns.php?beginning=";
-    //ipconfig car l'adresse local ne fonctionne pas quand il y a deux appareils différents
+    //ipconfig/all
     HttpURLConnection co;
     URL url;
     BufferedReader br;
@@ -36,45 +36,39 @@ public class MainActivity extends Activity {
         town = (EditText) findViewById(R.id.ET_town);
         is = null;
         policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
     }
+
+
     private String getServerDataRawText(String urlWebService){
         StringBuilder str = new StringBuilder();
         String line;
-
         StrictMode.setThreadPolicy(policy);
         try {
-
             // échange http avec le serveur
             url = new URL(urlWebService);
             co.connect();
             is = co.getInputStream(); // réponse HTTP
             co = (HttpURLConnection)url.openConnection();
+
             // exploitation/analyse de la réponse
             br = new BufferedReader(new InputStreamReader(is));
+
             while ((line = br.readLine()) !=null) {
-                str.append(" - ").append(line).append("\n");
+                str.append(" - ").append(line).append("\n"); // concatenation
             }
         } catch (Exception exception){
             Log.e("log_tag", "Error during data reading :" + exception.toString());
         }
         return str.toString();
-
-        // must have a return statement
     }
+
+
     @SuppressLint("SetTextI18n")
     public void Search(View view){
         urlWebService += town.getText();
         results.setText("Towns beginning with " + town.getText() + " : " );
         results.append(getServerDataRawText(urlWebService));
-
-
-
-        //if (town.getText().toString().matches("")) {
-          //  Toast.makeText(this, "No town searched", Toast.LENGTH_LONG).show();
-           // return;
-        //}
-        // results.append("\n Towns beginning with " + town.getText()  + ":\n");
-        // show off the result : results.append(mySQLiteDB.townBeginningWith(town.getText().toString()));
     }
+
+
 }
